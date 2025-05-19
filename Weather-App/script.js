e2984c6e5be19a1b65f9a2c9c3fef101bde8154c
jessi-17 <jaspreet.bhagat17@gmail.com>
2025-05-20 00:46:53 +0530

    do the changes

--- Weather-App/script.js
+++ Weather-App/script.js
@@ -1,82 +1,65 @@
-const input = document.getElementById("search-input");
-const description = document.getElementById("description-text");
-const img = document.getElementById("description-img");
-const locBtn = document.getElementById("loc-btn");
-const section = document.querySelector("section");
-const form = document.querySelector("form");
+const search = document.getElementById("btn");
+const inputQuery = document.querySelector("input[type='text']");
+const localtime = document.getElementById("localtime");
+const weatherContainer = document.getElementById("weather-Container");
+//fetch call: - http://api.weatherapi.com/v1/current.json?key=4daf6956a8314ec3b01183517251905&q=nakodar
 
-function getData(e) {
-  e.preventDefault();
-
-  if (!input.value) {
-    alert("Please Enter a city name");
-    return;
-  } else {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=ca695dcbc66c5fa3d0cb955033fd918f`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log(data);
-        displayWeather(data);
-        document.getElementById("city").style.display = "block";
-      });
-  }
+weatherContainer.innerHTML=''
+function DisplayReport(data){
+    localtime.innerHTML = data.location.localtime;
+    localtime.style.fontSize="1rem";
+    weatherContainer.innerHTML = `
+      <div class="weather-report">
+          <div class="city">
+              <h1>${data.location.name}</h1>
+          </div>
+          <div class="temperature">
+              <h1>${data.current.temp_c}°C</h1>
+          </div>
+          <div class="visual-data">
+              <img src="${data.current.condition.icon}" alt="cloud">
+              <h1>${data.current.text}</h1>
+          </div>
+          <div class="Detailed-information">
+              <div class="humidity">
+                  <div class="row-1">
+                      <i class="fa-solid fa-temperature-three-quarters"></i>
+                      <h2>Humidity</h2>
+                  </div>
+                  <div class="row-2">
+                      <h1>${data.current.humidity}</h1>
+                  </div>
+              </div>
+              <div class="feelslike">
+                  <div class="row-1">
+                      <i class="fa-solid fa-droplet"></i>
+                      <h2>Feels like</h2>
+                  </div>
+                  <div class="row-2">
+                      <h1>${data.current.feelslike_c}°C</h1>
+                  </div>
+              </div>
+          </div>
+      </div>
+      
+      `;
 }
-
-function getLocationData() {
-  if (!navigator.geolocation) {
-    alert("geolocation is not supported!");
-    return;
-  } else {
-    navigator.geolocation.getCurrentPosition((position) => {
-      fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=ca695dcbc66c5fa3d0cb955033fd918f`
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          // console.log(data);
-          displayWeather(data);
-          document.querySelector("header h5").style.display = "none";
-          document.getElementById("city").textContent = "current location";
-        });
-    });
-  }
+search.addEventListener("click", async () => {
+const location = inputQuery.value.trim();
+if(!location){
+    alert("Enter a location to search");
+    return; 
 }
-
-addEventListener("load", () => {
-  const date = document.getElementById("date");
-
-  const d = new Date();
-  let currentDate = d.toString().slice(4, 15);
-
-  date.innerHTML = currentDate;
+const url = `http://api.weatherapi.com/v1/current.json?key=4daf6956a8314ec3b01183517251905&q=${location};`
+try{
+    const response = await fetch(url);
+    if(!response.ok){
+        throw new Error("Failed to fetch data");
+    }
+    const data = await response.json();
+    DisplayReport(data);
+}catch(error){
+    console.log(error);
+    alert("Could not reterieve weather data. please try again.")
+}
 });
-
-function displayWeather(data) {
-  document.querySelector("header h5").style.display = "block";
-
-  const temp = (data.main.temp - 273.15).toFixed(1);
-  console.log(temp);
-
-  document.getElementById("temperature-degree").textContent = temp + "°";
-  document.getElementById("city").textContent = input.value;
-
-  document.getElementById("humidity-degree").textContent =
-    data.main.humidity + " %";
-  document.getElementById("feelslike-degree").textContent =
-    (data.main.feels_like - 273.15).toFixed(1) + " °";
-
-  description.textContent = data.weather[0].description;
-
-  img.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
-
-  section.style.display = "block";
-  section.classList.add("test");
-  document.getElementById("city").style.display = "block";
-  locBtn.style.display = "none";
-
-  // console.log(document.getElementById("city"));
-}
-
-form.addEventListener("submit", getData);
