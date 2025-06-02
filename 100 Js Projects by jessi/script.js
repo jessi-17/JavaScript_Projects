5931465ade26d082d5849eebf5bcdb8dfc898d03
jessi-17 <142052456+jessi-17@users.noreply.github.com>
2025-06-03 00:32:58 +0530

    do the changes

---
+++ 100 Js Projects by jessi/script.js
@@ -0,0 +1,343 @@
+window.onload = function (){
+    let arr = [
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+        ,
+        {
+            image_url:"images/cell-1.png",
+            title: "Meow Calculator",
+            git_link:"https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+            live_demo:"https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+        }
+    ];
+    
+    let card_container = document.getElementById("cardBox")
+    arr.forEach(function(e){
+        card_container.innerHTML += `
+        <div class="card">
+            <div class="image">
+                <img src="${e.image_url}" alt="">
+            </div>
+            <div class="text">
+                <h3>${e.title}</h3>
+                <div class="buttons">
+                    <a href="${e.git_link}">
+                        <button>Gitlink</button>
+                    </a>
+                    <a href="${e.live_demo}">
+                        <button>livedemo</button>
+                    </a>
+                </div>
+            </div>
+        </div>
+        
+        `
+    
+    })
+}
+
